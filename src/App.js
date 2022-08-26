949400f9ccd39bfcb4b004233366e165d8c3fc5a
Ibraxyz <yulizarsofyan14@gmail.com>
2022-08-26 15:28:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import PButton from "./components/PButton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PButton>Load More</PButton>
     </div>
   );
 }
