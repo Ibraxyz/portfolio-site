53da1c23a5d465499fd1667b9437b2a7c364109c
Ibraxyz <yulizarsofyan14@gmail.com>
2022-08-27 12:27:05 +0700

    add hover effect on SidemenuButton

--- src/components/SidemenuButton.js
+++ src/components/SidemenuButton.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Group from './Group';
 import styled from 'styled-components';
 
-const SidemenuButton = ({title,url}) => {
+const SidemenuButtonUnstyled = ({ title, url, className }) => {
     return (
         <Group bottom>
-            <div style={{ boxSizing: 'border-box', padding: '15px' }}>
+            <div style={{ boxSizing: 'border-box', padding: '15px' }} className={className}>
                 <p>{title}</p>
             </div>
         </Group>
     )
 }
+
+const SidemenuButton = styled(SidemenuButtonUnstyled)`
+&:hover {
+    color: #F54E4E; 
+    cursor:pointer;
+}
+`;
 
 export default SidemenuButton;
