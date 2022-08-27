37c752b1100fd8650e11ed6062d176bc7b4cbd9b
Ibraxyz <yulizarsofyan14@gmail.com>
2022-08-27 08:58:16 +0700

    change theme to dark

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
 import './App.css';
 import './styles/grid.css';
 
-import Button from "./components/Button";
 import Container from "./components/Container";
 import Group from './components/Group';
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <div className='col-3'>
-          <Group top left bottom right={false}>
-              <p>Lorem ipsum dolor sit amet this text only used as a placeholder to demonstrate an element taking space on a website.</p>
+    <div>
+      <Group top left bottom right>
+        <div className='col-12 no-gutter'>
+          <div style={{ boxSizing: 'border-box', paddingLeft: '15px', paddingRight: '15px' }}>
+            <h3>MYZ</h3>
+          </div>
+        </div>
+      </Group>
+
+      <Group left bottom right>
+        <div className='col-2 no-gutter'>
+          <Group>
+            <Group bottom>
+              <div style={{ boxSizing: 'border-box', padding: '15px' }}>
+                <p>About</p>
+              </div>
+            </Group>
+            <Group bottom>
+              <div style={{ boxSizing: 'border-box', padding: '15px' }}>
+                <p>Projects</p>
+              </div>
+            </Group>
+            <Group bottom>
+              <div style={{ boxSizing: 'border-box', padding: '15px' }}>
+                <p>Resume</p>
+              </div>
+            </Group>
+            <Group bottom>
+              <div style={{ boxSizing: 'border-box', padding: '15px' }}>
+                <p>Contact</p>
+              </div>
+            </Group>
           </Group>
         </div>
-      </Container>
+        <div className='col-10 no-gutter'>
+          <Group left>
+            <Container>
+              <div style={{ boxSizing: 'border-box', padding: '40px 60px 40px 60px', height: '720px' }}>
+                <img src={{}} alt="" />
+              </div>
+            </Container>
+          </Group>
+        </div>
+      </Group>
+
+      <Group left bottom right>
+        <div className='col-12 no-gutter'>
+          <Group>
+            <div style={{ boxSizing: 'border-box', padding: '15px' }}>
+              <p>Social Media</p>
+            </div>
+          </Group>
+        </div>
+      </Group>
     </div>
   );
 }
--- src/components/Group.js
+++ src/components/Group.js
@@ -3,91 +3,14 @@ import styled from 'styled-components';
 
 const Div = styled.div`
 box-sizing:border-box;
-border-left:5px solid rgba(0,0,0,0.5);
-border-top:5px solid rgba(0,0,0,0.5);
-border-bottom:5px solid rgba(0,0,0,0.5);
-border-right:5px solid rgba(0,0,0,0.5);
-padding:15px;
+border-left:${props => props.left ? "2px solid rgba(207, 216, 220, 0.1)" : "none"};
+border-top:${props => props.top ? "2px solid rgba(207, 216, 220, 0.1)" : "none"};
+border-bottom:${props => props.bottom ? "2px solid rgba(207, 216, 220, 0.1)" : "none"};
+border-right:${props => props.right ? "2px solid rgba(207, 216, 220, 0.1)" : "none"};
+display:inline-block;
+width:100%;
 `;
 
-const DivWithoutTop = styled.div`
-box-sizing:border-box;
-border-left:5px solid rgba(0,0,0,0.5);
-border-top:none;
-border-bottom:5px solid rgba(0,0,0,0.5);
-border-right:5px solid rgba(0,0,0,0.5);
-padding:15px;
-`;
-
-const DivWithoutBottom = styled.div`
-box-sizing:border-box;
-border-left:5px solid rgba(0,0,0,0.5);
-border-top:5px solid rgba(0,0,0,0.5);
-border-bottom:none;
-border-right:5px solid rgba(0,0,0,0.5);
-padding:15px;
-`;
-
-const DivWithoutLeft = styled.div`
-box-sizing:border-box;
-border-left:none;
-border-top:5px solid rgba(0,0,0,0.5);
-border-bottom:5px solid rgba(0,0,0,0.5);
-border-right:5px solid rgba(0,0,0,0.5);
-padding:15px;
-`;
-
-const DivWithoutRight = styled.div`
-box-sizing:border-box;
-border-left:5px solid rgba(0,0,0,0.5);
-border-top:5px solid rgba(0,0,0,0.5);
-border-bottom:5px solid rgba(0,0,0,0.5);
-padding:15px;
-border-right:none;
-`;
-
-const DivWithoutLeftAndRight = styled.div`
-box-sizing:border-box;
-border-left:none;
-border-top:5px solid rgba(0,0,0,0.5);
-border-bottom:5px solid rgba(0,0,0,0.5);
-border-right:none;
-padding:15px;
-`;
-
-const DivWithoutBottomAndTop = styled.div`
-box-sizing:border-box;
-border-left:5px solid rgba(0,0,0,0.5);
-border-top:none;
-border-bottom:none;
-border-right:none;
-padding:15px;
-`;
-
-const Group = ({top,left,bottom,right,children}) => {
-    if(top === false){
-        if(bottom === false){
-            return <DivWithoutBottomAndTop>{children}</DivWithoutBottomAndTop>
-        }
-        return <DivWithoutTop/>
-    }else if(bottom === false){
-        if(top === false){
-            return <DivWithoutBottomAndTop>{children}</DivWithoutBottomAndTop>
-        }
-        return <DivWithoutBottom/>
-    }else if(left === false){
-        if(right === false){
-            return <DivWithoutLeftAndRight>{children}</DivWithoutLeftAndRight>
-        }
-        return <DivWithoutLeft/>
-    }else if(right === false){
-        if(left === false){
-            return <DivWithoutLeftAndRight>{children}</DivWithoutLeftAndRight>
-        }
-        return <DivWithoutRight>{children}</DivWithoutRight>
-    }else{
-        return <Div>{children}</Div>
-    }
-}
+const Group = ({ top, left, bottom, right, children }) => <Div top={top} left={left} bottom={bottom} right={right}>{children}</Div>
 
 export default Group;
